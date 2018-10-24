7bcfd16c5caeb5968c4fe33cc2aee37daf79b30c
Jasper <j.drewes@telfort.nl>
2018-10-24 09:01:33 +0200

    styled the login page

---
+++ header.php
@@ -0,0 +1,16 @@
+	<div class="row">
+		<div class="col col-full-width">
+			<div class="wrapper-logo">
+				<a href="index.php"><img src="images/logo_jutter.png" alt="Logo Jutter"/></a>
+			</div>
+			
+			<div class="menu-trigger"><i class="fa fa-bars"></i></div>
+			
+			<nav class="style-nav">
+				<ul>
+					<li><a href="login.php">Login</a></li>
+					<li><a href="register.php">Register</a></li>
+				</ul>
+			</nav>
+		</div>
+	</div>
